f7f3151eece18681bc6766cf54a0a130fcef7974
Brando Lugo <brando.lugo@gmail.com>
2020-12-16 12:22:02 -0600

    Initial Commit

---
+++ Shared/ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionsApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 12/16/20.
+//
+
+import SwiftUI
+
+@main
+struct ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
