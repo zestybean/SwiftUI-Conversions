0ad30dd543d3710ed309eba375bd60ff44bc3f13
Brando Lugo <brando.lugo@gmail.com>
2020-12-16 15:03:01 -0600

    Initial project start

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputConversionNumber = ""
+    @State private var conversionChoice = 0
+    
+    @State private var subConversionChoice = 0
+    
+    var conversionResult = 0.0
+    
+    let conversionOptions = ["Length", "Temp", "Area", "Volume", "Weight"]
+    
+    let conversionOptionLength = ["Meter", "Kilometer", "Centimeter", "Millimeter", "Micrometer", "Nanometer", "Mile", "Yard", "Foot", "Inch", "Light Year"]
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("Choose a conversion option")) {
+                    
+                    Picker("Conversion Options", selection: $conversionChoice) {
+                        ForEach(0..<conversionOptions.count) {
+                            Text("\(self.conversionOptions[$0])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Input your conversion number and type")) {
+                    TextField("Input - Conversion", text: $inputConversionNumber)
+                        .keyboardType(.decimalPad)
+                    Picker("Sub Conversion Options", selection: $subConversionChoice) {
+                        ForEach(0..<conversionOptionLength.count) {
+                            Text("\(self.conversionOptionLength[$0])")
+                        }
+                    }
+                    .pickerStyle(WheelPickerStyle())
+                }
+                
+                Section(header: Text("Conversion Results")) {
+                    ForEach(0..<conversionOptionLength.count) {
+                        Text("\(self.conversionOptionLength[$0]): \(conversionResult)")
+                    }
+                }
+                
+                
+            }
+            .navigationTitle("Conversion Calculator")
+        }
+        
     }
 }
 
