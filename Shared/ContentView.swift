5e5163bec3696d1d5ad7e6c2c09ce851c641840d
Brando Lugo <brando.lugo@gmail.com>
2020-12-17 11:49:26 -0600

    Project simplified and completed needs refactoring!

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,38 +9,87 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var inputConversionNumber = ""
-    @State private var conversionChoice = 0
+    @State private var inputConversionChoice = 0
+
+    let inputConversionOptions = ["Celsius", "Kelvin", "Fahrenheit"]
     
-    @State private var subConversionChoice = 0
+    @State private var outputConversionChoice = 0
     
-    var conversionResult = 0.0
+    let outputConversionOptions = ["Celsius", "Kelvin", "Fahrenheit"]
     
-    let conversionOptions = ["Celsius", "Kelvin", "Fahrenheit", "Volume", "Weight"]
+    var conversionResults: Double {
+        let inputTemp = Double(inputConversionNumber) ?? 0
+        
+        if(inputConversionChoice == 0 && outputConversionChoice == 0) {
+            //Celsius to Celsius
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.celsius)
+            return temp.converted(to: UnitTemperature.celsius).value
+        } else if(inputConversionChoice == 0 && outputConversionChoice == 1) {
+            //Celsius to Kelvin
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.celsius)
+            return temp.converted(to: UnitTemperature.kelvin).value
+        } else if(inputConversionChoice == 0 && outputConversionChoice == 2) {
+            //Celsius to Fahrenheit
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.celsius)
+            return temp.converted(to: UnitTemperature.fahrenheit).value
+        }
+        
+        if(inputConversionChoice == 1 && outputConversionChoice == 0) {
+            //Celsius to Celsius
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.kelvin)
+            return temp.converted(to: UnitTemperature.celsius).value
+        } else if(inputConversionChoice == 1 && outputConversionChoice == 1) {
+            //Celsius to Kelvin
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.kelvin)
+            return temp.converted(to: UnitTemperature.kelvin).value
+        } else if(inputConversionChoice == 1 && outputConversionChoice == 2) {
+            //Celsius to Fahrenheit
+            let celTemp = Measurement.init(value: inputTemp, unit: UnitTemperature.kelvin)
+            return celTemp.converted(to: UnitTemperature.fahrenheit).value
+        }
+        
+        if(inputConversionChoice == 2 && outputConversionChoice == 0) {
+            //Celsius to Celsius
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.fahrenheit)
+            return temp.converted(to: UnitTemperature.celsius).value
+        } else if(inputConversionChoice == 2 && outputConversionChoice == 1) {
+            //Celsius to Kelvin
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.fahrenheit)
+            return temp.converted(to: UnitTemperature.kelvin).value
+        } else if(inputConversionChoice == 2 && outputConversionChoice == 2) {
+            //Celsius to Fahrenheit
+            let temp = Measurement.init(value: inputTemp, unit: UnitTemperature.fahrenheit)
+            return temp.converted(to: UnitTemperature.fahrenheit).value
+        }
+        
+        return 0
+    }
     
     var body: some View {
         NavigationView {
             Form {
-                Section(header: Text("Choose a conversion option")) {
+                Section(header: Text("Choose your intput temp type")) {
                     
-                    Picker("Conversion Options", selection: $conversionChoice) {
-                        ForEach(0..<conversionOptions.count) { choice in
-                            Text("\(self.conversionOptions[choice])")
+                    Picker("Conversion Options", selection: $inputConversionChoice) {
+                        ForEach(0..<inputConversionOptions.count) { choice in
+                            Text("\(self.inputConversionOptions[choice])")
                         }
                     }
-                    .onChange(of: conversionChoice) {_ in
-                        print(conversionChoice)
-                    }
                     .pickerStyle(SegmentedPickerStyle())
-                }
-                
-                Section(header: Text("Input your conversion number and type")) {
+                    
                     TextField("Input - Conversion", text: $inputConversionNumber)
                         .keyboardType(.decimalPad)
                 }
-                
+
                 Section(header: Text("Conversion Results")) {
-                 
-                        Text("\(conversionResult)")
+                    Picker("Conversion Options", selection: $outputConversionChoice) {
+                        ForEach(0..<outputConversionOptions.count) { choice in
+                            Text("\(self.outputConversionOptions[choice])")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    Text("Result: \(conversionResults, specifier: "%.1f")")
                  
                 }
                 
